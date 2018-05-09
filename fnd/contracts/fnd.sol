da2774dd2e25a7697e76c71a97e70553f2414d02
avgupta <av1659@gmail.com>
2018-05-09 01:26:41 -0700

    improved contract to add voting/reporting

--- fnd/contracts/fnd.sol
+++ fnd/contracts/fnd.sol
@@ -1,7 +1,7 @@
 pragma solidity ^0.4.21;
 
 contract FakeNewsMarket {
- mapping (address => uint256) public votes;
+ mapping (address => uint256) votes;
  address creator;
  uint256 article;
  uint256 votingPeriod;
@@ -9,11 +9,13 @@ contract FakeNewsMarket {
  function FakeNewsMarket(string _article) public {
    creator = msg.sender;
    article = sha256(_article);
-   votingTime = now + 1 days; //arbitrarily set to one day
+   votingPeriod = now + 1 days; //arbitrarily set to one day
  }
 
- function voteOnArticle(bool _vote) public{
+ function voteOnArticle(uint256 _vote) public{
+   require(_vote >= 0 && _vote < 3);
    require(msg.sender != creator);
    votes[msg.sender] == _vote;
+
  }
 }
---
+++ fnd/contracts/voting.sol
@@ -0,0 +1,74 @@
+pragma solidity ^0.4.22;
+
+contract FakeNewsMarket {
+
+    struct Voter {
+        bool voted;  // if true, that person already voted
+        uint vote;   // 0,1,2
+    }
+
+    struct Reporter {
+        bool voted;  // if true, that person already voted
+        uint vote;   // 0,1,2
+        uint weight;
+        uint reputation;
+    }
+
+    uint256 public articleHash;
+    //uint256 public votingPeriod;
+    address public creator;
+
+    mapping(address => Voter) public voters;
+    mapping(address => Reporter) public reporters;
+
+    uint256[3] votes = [0,0,0];
+    uint256[3] reports = [0,0,0];
+
+    function FakeNewsMarket(string _article) public {
+        creator = msg.sender;
+        articleHash =  sha256(_article);
+        //votingPeriod = now + 1 days;
+    }
+
+    function assignReporter(address reporter) public {
+        require(
+            msg.sender == creator,
+            "Only creator can assign reporters."
+        );
+        require(
+            !reporters[reporter].voted,
+            "The reporter already voted."
+        );
+        require(reporters[reporter].weight == 0);
+        reporters[reporter].weight = 1;
+    }
+
+    function report(uint _vote, uint256 _amount) public {
+        Reporter storage sender = reporters[msg.sender];
+        require(!sender.voted, "Already voted.");
+        sender.voted = true;
+        sender.vote = _vote;
+        reports[_vote] += reporter.weight;
+    }
+
+    function vote(uint _vote, uint256 _amount) public {
+        Voter storage sender = voters[msg.sender];
+        require(!sender.voted, "Already voted.");
+        sender.voted = true;
+        sender.vote = _vote;
+        votes[_vote] += 1;
+    }
+
+    function winningReport() public view
+            returns (uint winningReport_)
+    {
+        uint winningVoteCount = 0;
+        for (uint p = 0; p < reports.length; p++) {
+            if (reports[p] > winningVoteCount) {
+                winningVoteCount = reports[p];
+                winningReport_ = p;
+            }
+        }
+    }
+
+}
