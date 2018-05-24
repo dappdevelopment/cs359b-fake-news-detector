c4e00fe7977dd0230d574a36563bda0e1762c531
avgupta <av1659@gmail.com>
2018-05-23 23:04:03 -0700

    initial report and assignReporters

--- contracts/contracts/FakeNewsMarket.sol
+++ contracts/contracts/FakeNewsMarket.sol
@@ -2,7 +2,8 @@ pragma solidity ^0.4.22;
 
 contract FakeNewsMarket {
 
-    mapping(address => bytes32) reporters;
+    mapping(address => bytes32) reportersEmails; //address to email
+    address[] reporters;
 
     struct Bet {
         uint vote;   // 0,1,2
@@ -36,6 +37,10 @@ contract FakeNewsMarket {
           sum_votes: [uint(0),uint(0),uint(0)],
           sum_reports: [uint(0),uint(0),uint(0)]
       });
+      address[10] reporterAddresses = assignReporters();
+      for (uint i = 0; i < 10; i++){
+        new_market.reporters[reporterAddresses[i]] = Report({valid: true}); //initialize reports
+      }
       markets[article_hash] = new_market;
 
       emit ArticleCreated(msg.sender, initLen, article_hash);
@@ -46,9 +51,9 @@ contract FakeNewsMarket {
     // vote function
     // check if sender's address is already in array, if so, change existing
     // add address and money to map
-    bytes32 article_hash = keccak256(_article);
-    ArticleMarket market = markets[article_hash];
-    if (market.creator != 0) { //market exists
+      bytes32 article_hash = keccak256(_article);
+      ArticleMarket market = markets[article_hash];
+      if (market.creator != 0) { //market exists
         if ()
         Bet new_bet = Bet({
             vote: _vote,
@@ -59,23 +64,36 @@ contract FakeNewsMarket {
     }
 
     function report(string _article, uint _vote, uint _rep) public {
-      ArticleMarket market = markets[keccak256(_article)];
-      if (!market.reporters[msg.sender].valid) {
-        revert("You are not allowed to report!");
-      }
-      else {
-        market.reporters[msg.sender] = Report({
-          vote : _vote,
-          reputation : _rep,
-          valid: true
-        });
-      }
+      bytes32 hash = keccak256(_article);
+      require(markets[hash].reporters[msg.sender].valid == true);
+      markets[hash].reporters[msg.sender] = Report({
+        vote : _vote,
+        reputation : _rep,
+        valid: true
+      });
+      //what to do if updating reputation?
     }
 
-    // check if sender's address is in array, if not, block
-    // update report in map
-
     // assign reporters
+    function assignReporters() public returns (address[10] assigned){
+        uint[10] indices;
+        uint i = 0;
+        while (i < 10) {
+          uint random_number = uint(block.blockhash(block.number-1))%reporters.length + 1;
+          bool exists = false;
+          for (uint j = 0; j < i; j++) {
+            if(indices[j] == random_number) {
+              exists = true;
+            }
+          }
+          if (!exists){
+            indices[i] = random_number;
+            assigned[i] = reporters[random_number];
+            i += 1;
+          }
+        }
+        return assigned;
+    }
 
     // close market and distribute money
 
